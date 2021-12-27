2821c9972c10f45e67fbec4650ac60da1dd71895
Lu Ji <lu.ji1@me.com>
2021-12-27 16:45:24 -0500

    update strucutre

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const glob = require('glob');
 const defaultConfig = require("@wordpress/scripts/config/webpack.config");
 
-let entry = glob.sync('./inc/gutenberg/blocks/**/index.js').reduce((acc, path) => {
-	const entry = path.replace(/(\.\/inc\/gutenberg\/blocks\/)|(\/index.js)/g, "");
+let entry = glob.sync('./assets/js/gutenberg/blocks/**/index.js').reduce((acc, path) => {
+	const entry = path.replace(/(\.\/assets\/js\/gutenberg\/blocks\/)|(\/index.js)/g, "");
 	acc[entry] = path;
 	return acc;
 }, {});
